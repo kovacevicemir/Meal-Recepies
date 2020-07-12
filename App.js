a7ffc32fcca8795a5c5db0a5b034ef914b04d3e8
kovacevicemir <emirkovacevic92@gmail.com>
2020-07-13 08:35:44 +1000

    Screens & Fonts added

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import {AppLoading} from 'expo'
+
+const fetchFonts = () =>{
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setfontLoaded] = useState(false)
+  
+  if(!fontLoaded){
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
