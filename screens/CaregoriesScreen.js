0a2f73f5aa142a995b68e62ada0c5c723ac82b4d
kovacevicemir <emirkovacevic92@gmail.com>
2020-07-13 17:43:26 +1000

    playing with navigation and passing params basic

--- screens/CaregoriesScreen.js
+++ screens/CaregoriesScreen.js
@@ -1,23 +1,58 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
-const CaregoriesScreen = (props) => {
+const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
     return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-            <Button title='Go To Meals!' onPress={() => {
-                props.navigation.navigate({routeName:'CategoryMeals'})
-            }} />
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {categoryId: itemData.item.id}
+          });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
         </View>
-    )
-}
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+  );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+};
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+});
 
-export default CaregoriesScreen
+export default CategoriesScreen;
