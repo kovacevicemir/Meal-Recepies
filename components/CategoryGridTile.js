f373e3614928f4a6a52efd9c88d8d3af0c90779d
kovacevicemir <emirkovacevic92@gmail.com>
2020-07-15 13:39:03 +1000

    adding Header buttons

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -36,15 +36,17 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius:10,
+    elevation: 5,
+
     overflow:"hidden"
   },
   container: {
     flex: 1,
     borderRadius: 10,
     shadowColor: "black",
-    shadowOpacity: 0.6,
-    shadowOffset: { width: 0, height: 2 },
-    elevation: 3,
+    shadowOpacity: 0.7,
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius:10,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
