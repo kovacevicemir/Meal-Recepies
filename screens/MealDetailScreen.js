ebd3755e66de446e33c1573f57c990337b55430b
kovacevicemir <emirkovacevic92@gmail.com>
2020-07-17 19:03:31 +1000

    Modify Store: add action, then reducer logic and dispatch inside component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,9 @@
-import React, {useEffect} from "react";
-import {useSelector} from 'react-redux'
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
+import React, { useEffect, useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import { toggleFavourite } from "../store/actions/meals";
 
 const ListItem = (props) => {
   return (
@@ -21,14 +16,31 @@ const ListItem = (props) => {
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
 
-  const MEALS = useSelector(state => state.meals.meals)
+  const MEALS = useSelector((state) => state.meals.meals);
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
+  //MAKE THIS MEAL FAVOURITE
+  const dispatch = useDispatch();
+
+  const toggleFavouriteHandler = useCallback(() => {
+    dispatch(toggleFavourite(mealId));
+  },[dispatch, mealId]);
+
   //To prevent infinite loop
-  useEffect(()=>{
-    props.navigation.setParams({mealTitle:selectedMeal.title})
-  },[selectedMeal])
+  useEffect(() => {
+    props.navigation.setParams({ mealTitle: selectedMeal.title });
+    props.navigation.setParams({ toggleFav: toggleFavouriteHandler });
+  }, [selectedMeal, toggleFavouriteHandler]);
+
+  //Explanation:
+  //I am creating toggleFav. function that dispatch action...
+  //I want to trigger toggleFav. function inside my header
+  //I cant use useDispatch function outside component
+  //So i am passing params to Navigation
+  //Also i am using useEffect to prevent inf. loop because if u change nav it rerenders
+  //Also using useCallBack to prevent inf loop as useEffect watching toggleFav change
+  //So with useCallBack we are preventing toggleFav. to be recreated and avoiding inf.loop
 
 
   return (
@@ -52,19 +64,19 @@ const MealDetailScreen = (props) => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-
   //communicate with MealDetailScreen component
-  const title = navigationData.navigation.getParam('mealTitle')
- 
+  const title = navigationData.navigation.getParam("mealTitle");
+  const toggleFav = navigationData.navigation.getParam("toggleFav");
+
   return {
-    headerTitle: title ? title : 'loading',
+    headerTitle: title ? title : "loading",
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Favourite"
           iconName="ios-star"
           onPress={() => {
-            console.log("marked!");
+            toggleFav()
           }}
         />
       </HeaderButtons>
@@ -86,13 +98,13 @@ const styles = StyleSheet.create({
     fontSize: 22,
     textAlign: "center",
   },
-  listItem:{
-    marginVertical:10,
-    marginHorizontal:20,
-    borderColor:'#ccc',
-    borderWidth:1,
-    padding:10
-  }
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
 
 export default MealDetailScreen;
